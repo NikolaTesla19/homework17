597e2487e17cb5c6d7b6f4ea9238f89c82a40c0d
Denis <Denis.Efremov.ru@gmail.com>
2021-07-10 13:45:25 +0300

    prod 1.0.0

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 let string = prompt();
-
-function filter(str){
-  let vowelArr = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
+let vowelArr = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
+function filterFunction(str){
   let arr = str.split('');
   let newArr = arr.filter((element) => vowelArr.includes(element));
-  console.log('Количество гласных ' + newArr.length);
+  console.log('Количество гласных ' + newArr.length + ' (filter)');
 }
-filter(string);
+filterFunction(string);
+function forEachFunction(str){
+  let arr = str.split('');
+  let newArr = [];
+  arr.forEach(function(element){
+    if (vowelArr.includes(element)) {
+      newArr.push(element);
+    }
+  })
+  console.log('Количество гласных ' + newArr.length + ' (forEach)');
+};
+forEachFunction(string);
+function reduceFunction(str){
+  let arr = str.split('');
+  let newArr = [];
+  arr.reduce(function(accumulator, currentValue){
+    if (accumulator.includes(currentValue)) {
+      newArr.push(currentValue);
+    }
+    return accumulator;
+  }, vowelArr)
+  console.log('Количество гласных ' + newArr.length + ' (reduce)');
+}
+reduceFunction(string);
